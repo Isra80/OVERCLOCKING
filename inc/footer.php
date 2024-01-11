1f77342f814332036e547b4abf4a3ea4935421a8
Israel D <122309882+Isra80@users.noreply.github.com>
2024-01-10 19:46:24 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,45 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Encuentranos en:</h4>
+                <a href="#" target="_blank">
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+                <a href="#" target="_blank">
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="https://goo.gl/maps/uX9MB9fN3YbbMP1t5" target="_blank">
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos:</h4>
+                <p> 
+                Porque OVERCLOCKING cuenta con una amplia variedad de productos tecnológicos para uso empresarial y personal, contamos con la distribución autorizada de las mejores marcas.</br>
+                Porque contamos con excelentes precios, gracias a la relación cercana que tenemos con los fabricantes, permitiéndonos obtener los mejores precios.</br>
+                Porque queremos asegurarnos de que su proceso de compra sea lo más fácil y rápido posible, además de que encuentre el producto con las especificaciones que necesita para sus necesidades.</br>
+                Porque nuestro Site cuenta con total seguridad ofrecida por encriptación punto a punto SSL, además de contar con el respaldo de ser una empresa con más de 18 años en el mercado..</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Dirección</h4>
+                <p style="color: #FFF">Ecuador</p>
+                <p style="color: #FFF">Cayambe</p>
+                <p style="font-size:20px" aria-hidden="true">  +593–96 367 1941</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true">  06-2546900 / 06-2643298</p> <a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; tienda_overclocking@gmail.com</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">"OVERCLOCKING" &copy; 2022</h5>
+</footer>
